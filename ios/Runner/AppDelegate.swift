d9a35c9ed3304570b8dc79a80193ef016f3868cc
David Ogundepo <david.oludepo@gmail.com>
2022-03-13 21:14:03 +0000

    Beyond T

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,19 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+    
+    lazy var flutterEngine = FlutterEngine(name: "MyApp")
+    
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+      
+    UIApplication.shared.isStatusBarHidden = false
+      
+      flutterEngine.run()
+
+      GeneratedPluginRegistrant.register(with: self.flutterEngine)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
